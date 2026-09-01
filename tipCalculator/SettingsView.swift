ac3642e7e49ef2647ce01cd6ffcd51aba39a1070
Intelligencecore <07wood.temblor@icloud.com>
2026-09-01 13:37:05 -0400

    Commit Sept 1 - 3 minor changes

--- tipCalculator/SettingsView.swift
+++ tipCalculator/SettingsView.swift
@@ -5,19 +5,25 @@ import UIKit
 struct SettingsView: View {
 	
 	
-		// string connected to the calc output saying the currency (USD or EUR or CAD)
-		//	enum countryCurrency{
-		//		case USD(String)
-		//		case canadianDollar(String)
-		//		case euro(String)
-		//	}
+		// string connected to the calc output saying the currency (USD or
+	
+	enum countryCurrency{
+		
+		case cdefault(String)
+		case USD(String)
+		case AUD(String)
+		case MEX(String)
+		case GBP(String)
+		case EUR(String)
+	}
+	
 	
 	
 	@State private var showInfo = false
 	@State private var enableHapticFeedback: Bool = false
 	@State private var isVersionTapped: Bool = false
-	
-	
+	@State private var currencySelected: countryCurrency = .cdefault("USD")
+	@State private var showResetAlert:Bool = false
 	
 	var body: some View {
 		
@@ -25,116 +31,143 @@ struct SettingsView: View {
 			
 			List{
 				Button("Reset to default"){
-						// set the save data to zero
+					showResetAlert = true
 				}
+				.alert("Reset all values?", isPresented: $showResetAlert) {
+					Button("Cancel", role: .cancel) { }
+					Button("Reset", role: .destructive) {
+						currencySelected = .cdefault("USD")
+						
+					}
+				}
+			}
+			
+			
+			Text("Use another currency?")
+				.bold()
+				.padding()
+			
+			HStack {
 				
-			
+				Button{
+					currencySelected = .USD("USD")
+				}label: {
+					Text("USD")
+						.foregroundColor(Color.primary)
+				}
+				.background(RoundedRectangle(cornerRadius: 10)
+					.frame(width: 60, height: 40))
+				.scaledToFit()
+				.foregroundColor(Color.green)
+				.padding()
+				
+				Button{
+					currencySelected = .MEX("MEX")
+				}label: {
+					Text("MEX")
+						.foregroundColor(Color.primary)
 					
+				}
+				.background(RoundedRectangle(cornerRadius: 10)
+					.frame(width: 60, height: 40))
+				.scaledToFit()
+				.foregroundColor(Color.green)
+				.padding()
+				
+				Button{
+					currencySelected = .AUD("AUD")
+				}label: {
+					Text("AUD")
+						.foregroundColor(Color.primary)
+					
+				}
+				.background(RoundedRectangle(cornerRadius: 10)
+					.frame(width: 60, height: 40))
+				.scaledToFit()
+				.foregroundColor(Color.green)
+				.padding()
+				
+				Button{
+					currencySelected = .GBP("GBP")
+				}label: {
+					Text("GBP")
+						.foregroundColor(Color.primary)
+					
+				}
+				.background(RoundedRectangle(cornerRadius: 10)
+					.frame(width: 60, height: 40))
+				.scaledToFit()
+				.foregroundColor(Color.green)
+				.padding()
+				
+				Button{
+					currencySelected = .EUR("EUR")
+				}label: {
+					Text("EUR")
+						.foregroundColor(Color.primary)
+					
+				}
+				.background(RoundedRectangle(cornerRadius: 10)
+					.frame(width: 60, height: 40))
+				.scaledToFit()
+				.foregroundColor(Color.green)
+				.padding()
 			}
-			.frame(maxWidth: .infinity, alignment: .leading)
 			
-		}
-		
-		
-		Text("Use another currency?")
-			.bold()
-			.padding()
-		
-		HStack {
 			
-			Button{}label: {
-				Text("USD")
-					.foregroundColor(Color.white)
-			}
-			.background(RoundedRectangle(cornerRadius: 10)
-			.frame(width: 60, height: 40))
-			.scaledToFit()
-			.foregroundColor(Color.green)
-			.padding()
-			
-			Button{}label: {
-				Text("MEX")
-			}
-			.background(RoundedRectangle(cornerRadius: 10)
-			.frame(width: 60, height: 40))
-			.scaledToFit()
-			.padding()
-			
-			Button{}label: {
-				Text("AUD")
-			}
-			.background(RoundedRectangle(cornerRadius: 10)
-			.frame(width: 60, height: 40))
-			.scaledToFit()
-			.padding()
-			
-			Button{}label: {
-				Text("GBP")
-			}
-			.background(RoundedRectangle(cornerRadius: 10)
-			.frame(width: 60, height: 40))
-			.scaledToFit()
-			.padding()
-			
-		}
-		
-		
-		List{
 			
 			Toggle("Haptic Feedback?", isOn: $enableHapticFeedback)
 				// add the haptic call to this toggle
-		
+				.scrollDisabled(true)
+				.scrollContentBackground(.hidden)
+				.listRowBackground(Color.clear)
+				.padding(.top, 160)
+				.padding()
 			
-		}
-		.scrollDisabled(true)
-		.scrollContentBackground(.hidden)
-		.listRowBackground(Color.clear)
-		.padding(.top, 200)
-		
-		
-		
-		HStack {
-			Spacer()
-			Button {
-				showInfo = true
-			} label: {
-				HStack {
-					Text("Learn more about this App")
-					Image(systemName: "info.circle")
+			
+			HStack {
+				Spacer()
+				Button {
+					showInfo = true
+				} label: {
+					HStack {
+						Text("Learn more about this App")
+						Image(systemName: "info.circle")
+					}
+				}
+				.padding(.bottom)
+				
+				
+				
+			}
+			.padding(.horizontal)
+			.sheet(isPresented: $showInfo) {
+				AppInfo()
+			}
+			.navigationTitle("Settings")
+			.navigationBarTitleDisplayMode(.automatic)
+			.padding(.bottom, 0)
+			
+			
+				//versioning in small letters
+				// versioning comes from how many times i have done edits to the files
+			HStack{
+				Button {
+						// open the url of the github project
+					if let url = URL(string: "https://github.com/intelligencecore/tipCalculator") {
+						UIApplication.shared.open(url)
+					}
+				}
+				label: {
+					Text("Version: 0.8.1")
+						.bold()
+						.font(.footnote)
+						.foregroundStyle(Color.secondary)
 				}
 			}
-			.padding(.bottom)
-			
-			
+			.padding()
 			
 		}
-		.padding(.horizontal)
-		.sheet(isPresented: $showInfo) {
-			AppInfo()
-		}
-		.navigationTitle("Settings")
-		.navigationBarTitleDisplayMode(.automatic)
-		.padding(.bottom, 0)
-		
-		
-			//versioning in small letters
-			// versioning comes from how many times i have done edits to the files
-		HStack{
-			Button {
-					// open the url of the github project
-				if let url = URL(string: "https://github.com/intelligencecore/tipCalculator") {
-					UIApplication.shared.open(url)
-				}
-			}
-			label: {
-				Text("Version: 0.8")
-					.bold()
-					.font(.footnote)
-					.foregroundStyle(Color.secondary)
-			}
-		}
-		.padding()
-		
 	}
 }
 
